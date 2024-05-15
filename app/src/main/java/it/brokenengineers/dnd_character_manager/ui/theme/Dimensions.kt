fcfec23343808ae441911da00623ad20d665b95c
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2024-05-15 19:27:26 +0200

    Added predefined dimensions for padding

---
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/ui/theme/Dimensions.kt
@@ -0,0 +1,13 @@
+package it.brokenengineers.dnd_character_manager.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val SmallPadding = 8.dp
+val MediumPadding = 16.dp
+val LargePadding = 24.dp
+val XLPadding = 32.dp
+val XXLPadding = 48.dp
+
+val SmallVerticalSpacing = 8.dp
+val MediumVerticalSpacing = 16.dp
+val LargeVerticalSpacing = 24.dp
