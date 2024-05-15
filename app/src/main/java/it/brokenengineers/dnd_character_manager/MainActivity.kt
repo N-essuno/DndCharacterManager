a4c9da56cc9a3d14aa807336db78c0f6b7a8c430
marcooamato <amatomarco111@gmail.com>
2024-05-15 17:43:19 +0200

    Home page base structure done
    Co-authored-by: Gianluca Bàrmina <N-essuno@users.noreply.github.com>

--- app/src/main/java/it/brokenengineers/dnd_character_manager/MainActivity.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/MainActivity.kt
@@ -1,16 +1,28 @@
 package it.brokenengineers.dnd_character_manager
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import it.brokenengineers.dnd_character_manager.ui.theme.DndCharacterManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +31,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DndCharacterManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {innerPadding ->
+                    HomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +40,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomePage(modifier: Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){// body large text
+        Text("Welcome back adventurer!",
+            style = MaterialTheme.typography.titleLarge)
+
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            items(3){
+                CharacterCard()
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
+        }
+
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text("Create new character")
+        }
+    }
+
+}
+
+@Composable
+fun CharacterCard(){
+    Card{
+        Column{
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Character Image"
+            )
+            Text("Character Name")
+            Text("Character Class")
+            Text("Character Level")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     DndCharacterManagerTheme {
-        Greeting("Android")
+        HomePage(Modifier)
     }
 }
--- app/src/main/java/it/brokenengineers/dnd_character_manager/ui/theme/Type.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package it.brokenengineers.dnd_character_manager.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import it.brokenengineers.dnd_character_manager.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,8 +16,7 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -23,6 +24,7 @@ val Typography = Typography(
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    /* Other default text styles to override
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
