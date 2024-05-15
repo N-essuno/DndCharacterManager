627c7913aa95a4ecf7290d92798c56c6e83005a5
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2024-05-15 19:29:00 +0200

    Refactor adding new padding presents, fixed paddings, fixed overly button to create new PG

--- app/src/main/java/it/brokenengineers/dnd_character_manager/MainActivity.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/MainActivity.kt
@@ -1,6 +1,5 @@
 package it.brokenengineers.dnd_character_manager
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,13 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -25,8 +25,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import it.brokenengineers.dnd_character_manager.ui.theme.DndCharacterManagerTheme
+import it.brokenengineers.dnd_character_manager.ui.theme.MediumPadding
+import it.brokenengineers.dnd_character_manager.ui.theme.SmallPadding
+import it.brokenengineers.dnd_character_manager.ui.theme.XLPadding
+import it.brokenengineers.dnd_character_manager.ui.theme.XXLPadding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,28 +47,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomePage(modifier: Modifier){
-    Column(
-        // background color is the one from the theme
-        modifier = Modifier.fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){// body large text
-        Text("Welcome back adventurer!",
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-             modifier = Modifier.padding(16.dp)
-        )
-        LazyColumn(
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = XXLPadding)
+                .background(MaterialTheme.colorScheme.background),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            items(3){
-                CharacterCard()
-                Spacer(modifier = Modifier.padding(8.dp))
+        ) {
+            Text(
+                "Welcome back adventurer!",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.padding(top = XLPadding, bottom = MediumPadding)
+            )
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(5) {
+                    CharacterCard()
+                    Spacer(modifier = Modifier.padding(SmallPadding))
+                }
             }
         }
-
         Button(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(MediumPadding),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary,
+                contentColor = MaterialTheme.colorScheme.onSecondary
+            ),
             onClick = { /*TODO*/ }
         ) {
             Text("Create new character")
@@ -76,17 +92,32 @@ fun HomePage(modifier: Modifier){
 
 @Composable
 fun CharacterCard(){
-    Card(colors = CardDefaults.cardColors(
-        containerColor = MaterialTheme.colorScheme.surface
-    )){
-        Column{
+    Card(
+        modifier = Modifier
+            // to fill the width of the screen
+//            .fillMaxWidth()
+            .padding(start = MediumPadding, end = MediumPadding),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = "Character Image"
             )
-            Text("Character Name")
-            Text("Character Class")
-            Text("Character Level")
+            Text(
+                text = "Character Name",
+                modifier = Modifier.padding(start = SmallPadding, end = SmallPadding))
+            Text(
+                text = "Character Class",
+                modifier = Modifier.padding(start = SmallPadding, end = SmallPadding))
+            Text(
+                text = "Character Level",
+                modifier = Modifier.padding(start = SmallPadding, end = SmallPadding))
+            Spacer(modifier = Modifier.padding(SmallPadding))
         }
     }
 }
