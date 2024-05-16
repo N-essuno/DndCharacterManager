36cf45a3c9f371fe5d4d0c829c1243c138a2543c
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2024-05-16 18:21:04 +0200

    Added saving throws row with proficiencies, implemented dialog for short/long rest, started adding buttons forHP management

--- app/src/main/java/it/brokenengineers/dnd_character_manager/screens/character_sheet.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/screens/character_sheet.kt
@@ -11,14 +11,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -26,11 +35,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintLayoutScope
 import it.brokenengineers.dnd_character_manager.R
 import it.brokenengineers.dnd_character_manager.ui.theme.DndCharacterManagerTheme
 import it.brokenengineers.dnd_character_manager.ui.theme.LargePadding
-import it.brokenengineers.dnd_character_manager.ui.theme.MediumPadding
 import it.brokenengineers.dnd_character_manager.ui.theme.SmallPadding
 import it.brokenengineers.dnd_character_manager.ui.theme.XSPadding
 
@@ -42,12 +49,14 @@ fun CharacterSheet(modifier: Modifier) {
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(scrollState)
+            .padding(bottom = LargePadding)
     ) {
         Spacer(modifier = Modifier.height(LargePadding))
         ConstraintLayout(
             modifier = Modifier.fillMaxSize()
         ){
-            val (head, charImage, mainInfo, abilityRow1, abilityRow2, skillsRow) = createRefs()
+            val (head, charImage, mainInfo, abilityRow,
+                skillsRow, savingThrowsTitle, savingThrowsRow, ) = createRefs()
             CharacterSheetHead(modifier = Modifier
                 .constrainAs(head) {
                     top.linkTo(parent.top)
@@ -55,7 +64,7 @@ fun CharacterSheet(modifier: Modifier) {
                     end.linkTo(parent.end)
                 }
             )
-            CharacterImage(modifier = Modifier
+            ImageAndDamageRow(modifier = Modifier
                 .constrainAs(charImage) {
                     top.linkTo(head.bottom)
                     start.linkTo(parent.start)
@@ -70,15 +79,33 @@ fun CharacterSheet(modifier: Modifier) {
                 }
             )
             AbilityRow(modifier = Modifier
-                    .constrainAs(abilityRow1) {
+                    .constrainAs(abilityRow) {
                         top.linkTo(mainInfo.bottom)
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     }
             )
-            SkillsRow(modifier = Modifier
+            SkillRow(modifier = Modifier
                 .constrainAs(skillsRow) {
-                    top.linkTo(abilityRow1.bottom)
+                    top.linkTo(abilityRow.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Text(
+                style = MaterialTheme.typography.titleLarge,
+                text = "Saving throws",
+                modifier = Modifier
+                    .padding(SmallPadding)
+                    .constrainAs(savingThrowsTitle) {
+                        top.linkTo(skillsRow.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            )
+            SavingThrowsRow(modifier = Modifier
+                .constrainAs(savingThrowsRow) {
+                    top.linkTo(savingThrowsTitle.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 }
@@ -108,6 +135,7 @@ fun CharacterSheetHead(modifier: Modifier) {
                     }
             )
             Text(
+                style = MaterialTheme.typography.bodyMedium,
                 text = "Character Race",
                 modifier = Modifier
                     .padding(SmallPadding)
@@ -117,6 +145,7 @@ fun CharacterSheetHead(modifier: Modifier) {
                     }
             )
             Text(
+                style = MaterialTheme.typography.bodyMedium,
                 text = "Character Class",
                 modifier = Modifier
                     .padding(SmallPadding)
@@ -157,32 +186,109 @@ fun HitPointsCard(modifier: Modifier) {
         ) {
             // TODO: get values based on character
             Text("140/140")
-            Text("Hit Points")
+            Text(
+                modifier = Modifier.padding(XSPadding),
+                text = "Hit Points"
+            )
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RestButton(modifier: Modifier) {
-    // square button
-    Button(
-        onClick = { /*TODO*/ },
-        modifier = modifier
+    val showDialog = remember { mutableStateOf(false) }
+    MaterialTheme(
+        shapes = Shapes(small = CircleShape)
     ) {
-        Image(
-            painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
-            contentDescription = "Rest")
+        Button(
+            // on click show dialog with two options: short rest and long rest
+            onClick = {
+                showDialog.value = true
+            },
+            modifier = modifier
+        ) {
+            Image(
+                painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
+                contentDescription = "Rest"
+            )
+        }
+    }
+
+    if (showDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                showDialog.value = false
+            },
+            title = { Text("Rest Options") },
+            text = { Text("Choose an option") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        // TODO: handle short rest
+                        showDialog.value = false
+                    }
+                ) {
+                    Text("Short Rest")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        // TODO handle long rest
+                        showDialog.value = false
+                    }
+                ) {
+                    Text("Long Rest")
+                }
+            }
+        )
     }
 }
 
 @Composable
-fun CharacterImage(modifier: Modifier) {
-    MyRow(modifier = modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Character Image",
-            modifier = modifier
-        )
+fun ImageAndDamageRow(modifier: Modifier) {
+    Row (
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxSize()
+    ) {
+        ConstraintLayout (
+            modifier = Modifier.fillMaxSize()
+        ){
+            val (charImage, hitButton, tempHpButton, recoverHpButton) = createRefs()
+            Image(
+                modifier = Modifier
+                    .padding(SmallPadding)
+                    .constrainAs(charImage) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    },
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Character Image"
+            )
+            MyButton(
+                modifier = Modifier
+                    .constrainAs(hitButton) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(recoverHpButton.top)
+                        end.linkTo(parent.end)
+                    },
+                text = "Hit",
+            ) {}
+            MyButton(
+                modifier = Modifier
+                    .constrainAs(tempHpButton) {
+                        top.linkTo(hitButton.bottom)
+                        bottom.linkTo(recoverHpButton.top)
+                        end.linkTo(parent.end)
+                    },
+                text = "Temp HP",
+            ) {}
+        }
+
+
     }
 }
 
@@ -227,7 +333,7 @@ fun MainInfo(modifier: Modifier) {
                         top.linkTo(parent.top)
                         end.linkTo(armorClass.start)
                     },
-                name = "INIT.",
+                name = "INITIA",
                 value = "+3"
             )
             MainInfoElem(
@@ -259,11 +365,12 @@ fun MainInfoElem(modifier: Modifier, name: String, value: String) {
                 style = MaterialTheme.typography.bodyLarge,
                 text = value,
                 modifier = Modifier
+                    .padding(XSPadding)
                     .constrainAs(valueText) {
-                    start.linkTo(parent.start)
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
-                }
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        end.linkTo(parent.end)
+                    }
             )
             Text(
                 style = MaterialTheme.typography.bodyMedium,
@@ -413,7 +520,7 @@ fun AbilityCard(modifier: Modifier, name: String, value: String, bonus: String) 
 }
 
 @Composable
-fun SkillsRow(modifier: Modifier) {
+fun SkillRow(modifier: Modifier) {
     val skills1 = listOf(
         "Acrobatics", "Animal Handling", "Arcana", "Athletics",
         "Deception", "History", "Insight", "Intimidation", "Investigation"
@@ -441,7 +548,7 @@ fun SkillsRow(modifier: Modifier) {
                     }
             ){
                 skills1.forEach {
-                    SkillCard(
+                    SkillBox(
                         name = it,
                         value = "+3"
                     )
@@ -456,7 +563,7 @@ fun SkillsRow(modifier: Modifier) {
                     }
             ) {
                 skills2.forEach {
-                    SkillCard(
+                    SkillBox(
                         name = it,
                         value = "+3"
                     )
@@ -467,7 +574,7 @@ fun SkillsRow(modifier: Modifier) {
 }
 
 @Composable
-fun SkillCard(name: String, value: String) {
+fun SkillBox(name: String, value: String) {
     Box (
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background),
@@ -501,7 +608,7 @@ fun SkillCard(name: String, value: String) {
 }
 
 @Composable
-fun MyRow(modifier: Modifier, function: @Composable () -> Unit) {
+fun SavingThrowsRow(modifier: Modifier) {
     Row (
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
@@ -510,8 +617,148 @@ fun MyRow(modifier: Modifier, function: @Composable () -> Unit) {
         ConstraintLayout(
             modifier = Modifier.fillMaxSize()
         ) {
-            function()
+            val (str, dex, con, int, wis, cha) = createRefs()
+            // TODO: get bonus based on character
+            SavingThrowCard(
+                name = "STRENGTH",
+                bonus = "+3",
+                modifier = Modifier
+                    .constrainAs(str) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        end.linkTo(dex.start)
+                    }
+            )
+            SavingThrowCard(
+                name = "DEXTERITY",
+                bonus = "+2",
+                modifier = Modifier
+                    .constrainAs(dex) {
+                        start.linkTo(str.end)
+                        top.linkTo(parent.top)
+                        end.linkTo(parent.end)
+                    }
+            )
+            SavingThrowCard(
+                name = "CONSTITUTION",
+                bonus = "+2",
+                modifier = Modifier
+                    .constrainAs(con) {
+                        start.linkTo(parent.start)
+                        top.linkTo(str.bottom)
+                        end.linkTo(int.start)
+                    }
+            )
+            SavingThrowCard(
+                name = "INTELLIGENCE",
+                bonus = "+0",
+                modifier = Modifier
+                    .constrainAs(int) {
+                        start.linkTo(con.end)
+                        top.linkTo(dex.bottom)
+                        end.linkTo(parent.end)
+                    }
+            )
+            SavingThrowCard(
+                name = "WISDOM",
+                bonus = "+1",
+                modifier = Modifier
+                    .constrainAs(wis) {
+                        start.linkTo(parent.start)
+                        top.linkTo(con.bottom)
+                        end.linkTo(cha.start)
+                    }
+            )
+            SavingThrowCard(
+                name = "CHARISMA",
+                bonus = "-1",
+                modifier = Modifier
+                    .constrainAs(cha) {
+                        start.linkTo(wis.end)
+                        top.linkTo(con.bottom)
+                        end.linkTo(parent.end)
+                    }
+            )
         }
+    }
+}
+
+@Composable
+fun SavingThrowCard(modifier: Modifier, name: String, bonus: String){
+    Card (
+        modifier = modifier
+            .fillMaxWidth(0.50f)
+            .padding(SmallPadding),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    ) {
+        ConstraintLayout (
+            modifier = Modifier.fillMaxSize()
+        ){
+            val (nameText, bonusText, proficiencyCheckBok) = createRefs()
+            RoundCheckbox(
+                modifier = Modifier
+                    .size(24.dp)
+                    .constrainAs(proficiencyCheckBok) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    }
+            )
+            Text(
+                style = MaterialTheme.typography.bodySmall,
+                text = name,
+                modifier = Modifier
+                    .constrainAs(nameText) {
+                        start.linkTo(proficiencyCheckBok.end)
+                        top.linkTo(parent.top)
+                        end.linkTo(bonusText.start)
+                        bottom.linkTo(parent.bottom)
+                    }
+            )
+            Text(
+                style = MaterialTheme.typography.bodySmall,
+                text = bonus,
+                modifier = Modifier
+                    .padding(XSPadding)
+                    .constrainAs(bonusText) {
+                        top.linkTo(parent.top)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }
+            )
+        }
+    }
+}
+
+@Composable
+fun RoundCheckbox(modifier: Modifier) {
+    // TODO: get checked based on character
+    val checkedState = remember { mutableStateOf(true) }
+    MaterialTheme(
+        shapes = Shapes(small = CircleShape)
+    ) {
+        Checkbox(
+            modifier = modifier,
+            checked = checkedState.value,
+            onCheckedChange = { },
+            colors = CheckboxDefaults.colors(
+                checkedColor = MaterialTheme.colorScheme.primary,
+                uncheckedColor = MaterialTheme.colorScheme.secondary
+            )
+        )
+    }
+}
+
+@Composable
+fun MyButton(modifier: Modifier, text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .padding(end = SmallPadding)
+    ) {
+        Text(text)
     }
 }
 
