8bd290b2d950309db592c50f188b1f2afaecbe72
marcooamato <amatomarco111@gmail.com>
2024-05-17 16:04:03 +0200

    Magic selection screen done + restyled ExpandableCard composable

--- app/src/main/java/it/brokenengineers/dnd_character_manager/screens/levelup/main_screen.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/screens/levelup/main_screen.kt
@@ -1,15 +1,26 @@
 package it.brokenengineers.dnd_character_manager.screens.levelup
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import it.brokenengineers.dnd_character_manager.ui.composables.CharacterCard
+import it.brokenengineers.dnd_character_manager.ui.composables.ExpandableCard
+import it.brokenengineers.dnd_character_manager.ui.theme.SmallPadding
 import it.brokenengineers.dnd_character_manager.ui.theme.XXLPadding
 
 @Composable
@@ -23,8 +34,49 @@ fun LevelUp(){
     ){
         item {CharacterCard()}
         item {Text(text = "Level Up!", style = MaterialTheme.typography.titleLarge) }
-        item { ArcaneTradition() }
-        item { AbilityScoreImprovement() }
-        item { NewFeatures() }
+        item { NewMagic() }
+//        item { ArcaneTradition() }
+//        item { AbilityScoreImprovement() }
+//        item { NewFeatures() }
     }
 }
+
+@Composable
+fun NewMagic() {
+    val context = LocalContext.current
+    val magicList = listOf(
+        MagicItem("Magic Missile", "A missile of magical energy darts forth from your fingertip and strikes its target, dealing 1d4+1 force damage."),
+        MagicItem("Fireball", "A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame."),
+        MagicItem("Lightning Bolt", "A stroke of lightning forming a line 100 feet long and 5 feet wide blasts out from you in a direction you choose."),
+        MagicItem("Cure Wounds", "A creature you touch regains a number of hit points equal to 1d8 + your spellcasting ability modifier."),
+    )
+    var selectedMagics by remember { mutableStateOf<List<MagicItem>>(emptyList()) }
+
+    Text("Choose 2 new magics", style = MaterialTheme.typography.titleMedium)
+    Spacer(modifier = Modifier.padding(SmallPadding))
+    magicList.forEach { magic ->
+        ExpandableCard(
+            title = magic.name,
+            description = magic.description,
+            selected = selectedMagics.contains(magic),
+            onSelected = {
+                selectedMagics = if (selectedMagics.contains(magic)) {
+                    // clicked on a selected magic, remove from selected
+                    selectedMagics - magic
+                }
+                else if (selectedMagics.size < 2 && !selectedMagics.contains(magic)) {
+                    // clicked on an unselected magic, add to selected
+                    selectedMagics + magic
+                } else {
+                    // toast to warn user that they can only select 2 magics
+                    Toast.makeText(context, "You can only select 2 magics", Toast.LENGTH_SHORT).show()
+                    selectedMagics
+                }
+                Log.d("NewMagic", selectedMagics.toString())
+            }
+        )
+        Spacer(modifier = Modifier.padding(SmallPadding))
+    }
+}
+
+data class MagicItem(val name: String, val description: String)
--- app/src/main/java/it/brokenengineers/dnd_character_manager/ui/composables/expandable_card.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/ui/composables/expandable_card.kt
@@ -2,13 +2,13 @@ package it.brokenengineers.dnd_character_manager.ui.composables
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -23,7 +23,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import it.brokenengineers.dnd_character_manager.ui.theme.SmallPadding
 
 @Composable
@@ -36,7 +35,8 @@ fun ExpandableCard(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { expanded = !expanded },
+            .clickable { expanded = !expanded }
+            .padding(SmallPadding),
         elevation =
         if (expanded) CardDefaults.elevatedCardElevation() // Increase elevation when expanded
         else CardDefaults.cardElevation(), // Default elevation when collapsed
@@ -47,43 +47,44 @@ fun ExpandableCard(
             else MaterialTheme.colorScheme.tertiary
         )
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(SmallPadding)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = title, style = MaterialTheme.typography.titleLarge)
-                Icon(
-                    imageVector = Icons.Filled.ArrowDropDown,
-                    contentDescription = "Expand or collapse",
-                    modifier = Modifier.rotate(if (expanded) 180f else 0f).
+            Text(text = title,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(SmallPadding))
+            Icon(
+                imageVector = Icons.Filled.ArrowDropDown,
+                contentDescription = "Expand or collapse",
+                modifier = Modifier.rotate(if (expanded) 180f else 0f).
 //                        weight(0.1f).
-                    padding(end = SmallPadding)
-                )
+                padding(end = SmallPadding)
+            )
 
-                Button(
-                    onClick = onSelected,
+            Button(
+                onClick = onSelected,
 //                    modifier = Modifier.size(40.dp)
-                ) {
-                    // show a plus icon
-                    Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = "Select",
-                        tint = Color.White
-                    )
-//                    Text(text = "Select")
-                }
+            ) {
+                // show a plus icon
+                Icon(
+                    imageVector =
+                        if (selected) Icons.Filled.Delete
+                        else Icons.Filled.Add,
+                    contentDescription = "Select",
+                    tint =
+                        if (selected) MaterialTheme.colorScheme.onPrimary
+                        else MaterialTheme.colorScheme.onSurface
+                )
             }
+        }
 
-            // Show the description if the card is expanded
-            if (expanded) {
-                Text(text = description, style = MaterialTheme.typography.bodyMedium)
-            }
+        // Show the description if the card is expanded
+        if (expanded) {
+            Text(text = description,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(SmallPadding))
         }
     }
 }
