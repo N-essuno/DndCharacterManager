6ad17977ff146be11be90e7ea66d69fe168e58c7
marcooamato <amatomarco111@gmail.com>
2024-05-18 19:11:44 +0200

    Bug in onAdd: the selected magic list is reset every time the onAdd finishes

--- app/src/main/java/it/brokenengineers/dnd_character_manager/screens/rest.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/screens/rest.kt
@@ -1,5 +1,6 @@
 package it.brokenengineers.dnd_character_manager.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,17 +10,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import it.brokenengineers.dnd_character_manager.ui.composables.CharacterCard
@@ -94,61 +100,89 @@ fun LongRest() {
 
 @Composable
 fun ShortRest() {
+    // should be retrieved from data model based on character selected
+    val magicsKnown = listOf(Magic("Magic1", 1), Magic("Magic2", 2), Magic("Magic3", 3), Magic("Magic4", 4))
+
     // selected magics contains map magic -> times selected
-    val selectedMagics = remember { mutableMapOf<Magic, Int>() }
+    val selectedMagics = remember { mutableStateMapOf<Magic, Int>() }
+    initSelectedMagics(selectedMagics, magicsKnown) // initialize the map with 0 for each magic
     val totalSlotsUsed = selectedMagics.values.sum()
     val slotsAvailable = 4
 
-    val magicsKnown = listOf(Magic("Magic1", 1), Magic("Magic2", 2), Magic("Magic3", 3), Magic("Magic4", 4))
 
     Column {
         Text("Choose magics to recover", style = MaterialTheme.typography.titleMedium)
         Text(text = "You have $slotsAvailable slots available", style = MaterialTheme.typography.bodyLarge)
         magicsKnown.forEach { magic ->
             MagicRow(
-                selectedMagics = selectedMagics,
+                selectedCount = selectedMagics[magic] ?: 0,
+                slotsAvailable = slotsAvailable,
                 magic = magic,
-                onAdd = { addToChosen(magic, selectedMagics) },
-                onRemove = { removeFromChosen(magic) })
+                onAdd = {
+                    if(totalSlotsUsed < slotsAvailable) {
+                        Log.d("ShortRest", "Adding ${magic.name}. Current count: ${selectedMagics[magic]}")
+                        selectedMagics[magic]?.let { currentCount ->
+                            selectedMagics[magic] = currentCount + 1
+                        }
+                        Log.d("ShortRest", "New count: ${selectedMagics[magic]}")
+                    }
+                },
+                onRemove = {
+                    if(selectedMagics[magic]!! > 0) {
+                        selectedMagics[magic]?.let {currentCount ->
+                            selectedMagics[magic] = currentCount - 1
+                        }
+                    }
+                }
+            )
         }
     }
 }
 
-fun removeFromChosen(magic: Magic) {
-
-}
-
-fun addToChosen(magic: Magic, selectedMagic: MutableMap<Magic, Int>) {
-    if(selectedMagic.containsKey(magic)){
-        // non null operator safe because we checked if it contains the key
-        selectedMagic[magic] = selectedMagic[magic]!! + 1
-    } else{
-        selectedMagic[magic] = 1
+fun initSelectedMagics(
+    selectedMagics: MutableMap<Magic, Int>,
+    magicsKnown: List<Magic>)
+{
+    magicsKnown.forEach { magic ->
+        selectedMagics[magic] = 0
     }
 }
 
 @Composable
 fun MagicRow(
-    selectedMagics: Map<Magic, Int>,
+    selectedCount: Int,
+    slotsAvailable: Int,
     magic: Magic,
     onAdd: () -> Unit,
     onRemove: () -> Unit
 ) {
+    val timesSelected by remember { derivedStateOf { selectedCount } }
+
     Row {
         Text(text = "${magic.name} (Level: ${magic.level})")
         Spacer(modifier = Modifier.weight(1f))
-        Button(onClick = onAdd) {
-            Text("Add")
+        Button(
+            onClick = onAdd,
+            enabled = selectedCount < slotsAvailable
+        ) {
+            Icon(
+                painter = painterResource(id = android.R.drawable.ic_input_add),
+                contentDescription = "Add"
+            )
         }
         Spacer(modifier = Modifier.width(8.dp))
-        if(selectedMagics.containsKey(magic)){
-            Text(text = selectedMagics[magic].toString())
-        } else{
-            Button(onClick = onRemove) {
-                Text("Remove")
-            }
-            Text(text = "0")
+
+        Button(
+            onClick = onRemove,
+            // if the magic is not in the map, the button should be disabled
+            enabled = selectedCount > 0
+        ) {
+            Icon(
+                painter = painterResource(id = android.R.drawable.ic_input_delete),
+                contentDescription = "Remove"
+            )
         }
+        Text(text = timesSelected.toString())
     }
 }
 
