2c0ba77d50afe89d1392a7ce8963aa95f5f40263
marcooamato <amatomarco111@gmail.com>
2024-05-21 17:45:19 +0200

    view model params are now enum

--- app/src/main/java/it/brokenengineers/dnd_character_manager/view_model/BuildCharacterViewModel.kt
+++ app/src/main/java/it/brokenengineers/dnd_character_manager/view_model/BuildCharacterViewModel.kt
@@ -2,26 +2,37 @@ package it.brokenengineers.dnd_character_manager.view_model
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import it.brokenengineers.dnd_character_manager.data.Character
+import it.brokenengineers.dnd_character_manager.data.DndClass
+import it.brokenengineers.dnd_character_manager.data.Race
+import it.brokenengineers.dnd_character_manager.data.enums.RaceEnum
+import it.brokenengineers.dnd_character_manager.data.enums.DndClassEnum
+import java.util.Locale
 
 class BuildCharacterViewModel: ViewModel(){
-    private var characterName: String = ""
-    private var characterRace: String = ""
-    private var characterClass: String = ""
+    private lateinit var characterName: String
+    private lateinit var characterRace: Race
+    private lateinit var characterClass: DndClass
     private var characterImage: Uri? = null
+    private var character: Character? = null
 
     fun setCharacterName(characterName: String) {
         this.characterName = characterName
     }
 
     fun setCharacterRace(characterRace: String) {
-        this.characterRace = characterRace
+        this.characterRace = RaceEnum.valueOf(characterRace.uppercase()).race
     }
 
     fun setCharacterClass(characterClass: String) {
-        this.characterClass = characterClass
+        this.characterClass = DndClassEnum.valueOf(characterClass.uppercase()).dndClass
     }
 
     fun setCharacterImage(it: Uri) {
         this.characterImage = it
+    }
+
+    fun createCharacter() {
+
     }
 }
