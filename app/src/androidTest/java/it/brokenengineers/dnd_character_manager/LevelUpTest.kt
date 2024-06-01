c4d7f693f177fd3b7040617647f3287041e933d7
marcooamato <amatomarco111@gmail.com>
2024-06-01 17:34:33 +0200

    started levelup test

---
+++ app/src/androidTest/java/it/brokenengineers/dnd_character_manager/LevelUpTest.kt
@@ -0,0 +1,9 @@
+package it.brokenengineers.dnd_character_manager
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class LevelUpTest {
+}
